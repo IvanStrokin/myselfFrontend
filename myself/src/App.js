d8f3ea6b5443289081ebdf9bd63cfd768dea9dcc
IvanStrokin <strokinIv23@yandex.ru>
2023-03-26 20:41:57 +0400

    firststep

---
+++ myself/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from './components/header/header'
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className="mainWrapper"> 
+                <Header />
+                
+            </div>
+        )
+    }
+     
+}
+
+export default App
---
+++ myself/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  './header'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="headerWrapper">
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+export default Header
+
