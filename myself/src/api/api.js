d22f3472b3a6966dfa866fd032828ae5f636a50a
IvanStrokin <strokinIv23@yandex.ru>
2023-05-16 00:41:23 +0400

    041

--- myself/src/api/api.js
+++ myself/src/api/api.js
@@ -1,28 +1,35 @@
-import { createApi,  fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseURL = "http://localhost:8081/"
 
 export const yasamApi = createApi({
-    reducerPath: "yasamApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: baseURL,
-      }),
-    endpoints: (builder) => ({
-        getUserById : builder.query({
-            query: (id) => `users/${id}`
-        }),
-        createUser: builder.mutation({
-            query: (body) => ({
-              url:  'users',
-              method: 'POST',
-              body,
-            }),
-            invalidatesTags : [ 'users' ],
-          }),
+  reducerPath: "yasamApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseURL,
+  }),
+  endpoints: (builder) => ({
+    getUserById: builder.query({
+      query: (id) => `users/${id}`
     }),
-});
 
-export const { useGetUsersQuery, useCreateUserMutation, useGetUserByIdQuery } = yasamApi
+    createUser: builder.mutation({
+      query: (body) => ({
+        url: 'users',
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: ['users'],
+    }),
+
+    getNKOById: builder.query({
+      query: (id) => `ncos/${id}`
+    }),
+
+  }),
+})
+
+
+export const {useCreateUserMutation, useGetUserByIdQuery, useGetNKOByIdQuery } = yasamApi
 
 
 
